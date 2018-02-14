188e8bbeae1c817258faa3d8ca521d068fbc5222
J.L <jl223vy@outlook.com>
2018-02-14 14:45:23 +0800

    upload

--- 基础练习/BASIC-29 高精度加法.cpp
+++ 基础练习/BASIC-29 高精度加法.cpp
@@ -2,45 +2,31 @@
 #include <cstdio>
 #include <cstring>
 using namespace std;
+
 const int maxn=105;
-char a[maxn],b[maxn],c[maxn];
+char sa[maxn],sb[maxn];
+int a[maxn]={0},b[maxn]={0},c[maxn]={0};
 int main(){
-	memset(c,'0',sizeof(c));
-	gets(a);
-	gets(b);
-	int aIsBigger=strlen(a)>strlen(b)?1:0;
-	int t,n,p=0;
-	if(aIsBigger){
-		for(int i=strlen(a)-1,j=strlen(b)-1;j>=0;i--,j--){
-			t=a[i]-'0'+(b[j]-'0');
-			n=c[p]-'0'+t;
-			if(n>=10){
-				c[p]=n%10+'0';
-				c[++p]='1';
-			}else{
-				c[p++]=n+'0';
-			}
-		}
-		for(int i=(int)strlen(a)-p-1;p<strlen(a);i--,p++){
-			c[p]=a[i];
-		}
-	}else{
-		for(int i=strlen(a)-1,j=strlen(b)-1;i>=0;i--,j--){
-			t=a[i]-'0'+(b[j]-'0');
-			n=c[p]-'0'+t;
-			if(n>=10){
-				c[p]=n%10+'0';
-				c[++p]='1';
-			}else{
-				c[p++]=n+'0';
-			}
-		}
-		for(int i=(int)strlen(b)-p-1;p<strlen(b);i--,p++){
-			c[p]=b[i];
-		}
+	scanf("%s%s",sa,sb);
+	int lena=strlen(sa);
+	int lenb=strlen(sb);
+	for(int i=lena-1,j=0;i>=0;i--,j++){
+		a[j]=sa[i]-48;
 	}
-	for(int i=p-1;i>=0;i--){
+	for(int i=lenb-1,j=0;i>=0;i--,j++){
+		b[j]=sb[i]-48;
+	}
+	int t=0,max;
+	for(int i=0;i<100;i++){
+		c[i]=a[i]+b[i]+t;
+		t=c[i]/10;
+		c[i]=c[i]%10;
+	}
+	max=lena>lenb?lena:lenb;
+	max=c[max]==0?max-1:max;
+	for(int i=max;i>=0;i--){
 		cout<<c[i];
 	}
 	return 0;
 }
+
